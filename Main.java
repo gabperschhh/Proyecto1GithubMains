20a5996485c274a1971217ea7c383268084e7efd
Gabriel Perez <gabpersch@gmail.com>
2025-10-02 16:06:46 -0600

    Inicializacion para pruebas (malio sal xd)

--- Main.java
+++ Main.java
@@ -1,3 +1,14 @@
 public class Main{
-    
+    public static void main(String[] args) {
+        Batalla b = new Batalla();
+        Inicializar i = new Inicializar();
+        i.inicializarElementos();
+        i.inicializarAtaques();
+        i.inicializarPokemones();
+        i.inicializarEntrenadores();
+        Entrenador jugador = i.inicializarJugador();
+        i.inicializarGimnasio();
+        Entrenador npc = i.poolEntrenadores[0];
+        b.cicloBatalla(jugador, npc);
+    }
 }
