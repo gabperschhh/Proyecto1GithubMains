b55e1b2b3110373d5cd7ad6341cbcbaaa09ffafb
Gabriel Perez <gabpersch@gmail.com>
2025-10-06 21:26:17 -0600

    indice entrenadores npc

--- Main.java
+++ Main.java
@@ -2,13 +2,14 @@ public class Main{
     public static void main(String[] args) {
         Batalla b = new Batalla();
         Inicializar i = new Inicializar();
+        int indiceNPC = 0;
         i.inicializarElementos();
         i.inicializarAtaques(); 
         i.inicializarPokemones();
         i.inicializarEntrenadores();
         Entrenador jugador = i.inicializarJugador();
         i.inicializarGimnasios();
-        Entrenador npc = i.poolEntrenadores[0];
+        Entrenador npc = i.poolEntrenadores[indiceNPC];
         b.cicloBatalla(jugador, npc);
     }
 }
